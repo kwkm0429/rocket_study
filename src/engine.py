27e92988ec61463a79d4ba72580cef38683767d1
Kosuke Kawakami <kwkm0429@gmail.com>
2022-05-18 23:31:41 +0900

    add ex_3_3

--- src/engine.py
+++ src/engine.py
@@ -5,14 +5,32 @@
 class Engine:
 	def __init__(self):
 		self.k = 1.3 #比熱比
-		self.mach_exit = 2.4 #出口のマッハ数
-		self.mach_throat = 1.0 #スロートのマッハ数
+		self.cp = 0 
+		self.R = 0
+		# 圧力
 		self.pc = 0 #燃焼室圧
 		self.pt = 0 #スロート圧
 		self.pe = 0 #出口圧
+		# 温度
 		self.tc = 0 #燃焼室温度
+		self.tt = 0 #スロート温度
+		self.te = 0 #出口温度
 		self.mass_dot = 0 #燃料消費率
+		# 面積
+		self.area_t = 0 #スロート面積
+		self.area_e = 0 #出口面積
 		self.ratio_area_throat_exit = 0 #スロートと出口の面積比
+		# 比容積
+		self.vol_t = 0 #スロート比容積
+		self.vol_t = 0 #燃焼室比容積
+		self.vol_e = 0 #出口比容積
+		# 速度
+		self.vel_t = 0 # スロート速度
+		self.vel_e = 0 # 出口速度
+		self.mach_exit = 2.4 #出口マッハ数
+		self.mach_throat = 1.0 #スロートマッハ数
+
 		self.time_combustion = 10 #燃焼時間
 		self.c_ave = 3038 #[m/s] 平均有効排気速度,\bar{c}
 		self.c_star = 0 #[m/s]
+		self.thrust = 0 #[N] 推力
--- src/main.py
+++ src/main.py
@@ -47,6 +47,38 @@ def ex_3_2(engine, earth):
 	graph_plot(pres, vel, "Pressure", "Velocity", "figs/Ex3_2-P-Vel.png")
 	graph_plot(pres, mach, "Pressure", "Mach", "figs/Ex3_2-P-M.png")
 
+def ex_3_3(engine, earth):
+	# input
+	engine.thrust = 5000
+	engine.pc = 2.068*1e6
+	engine.tc = 2800
+	engine.k = 1.30
+	engine.R = 355.4
+	earth.pressure = 0.002549*1e6
+	engine.pe = earth.pressure
+	# calculation
+	ratio_pt_pc = earth.pressure / engine.pc
+	engine.pt = engine.pc * (2/(engine.k+1))**(engine.k/(engine.k-1))
+	engine.tt = engine.tc * 2/(engine.k+1)
+	engine.vol_c = engine.R * engine.tc / engine.pc
+	engine.vol_t = engine.vol_c * ((engine.k+1)/2)**(1/(engine.k-1))
+	engine.vol_e = engine.vol_c * (engine.pc/engine.pe)**(1/engine.k)
+
+	# answer
+	engine.vel_t = math.sqrt(engine.k*engine.R*engine.tt)
+	engine.vel_e = engine.vel_t * math.sqrt((engine.k+1)/(engine.k-1)*(1-(engine.pe/engine.pc)**((engine.k-1)/engine.k)))
+	engine.mass_dot = engine.thrust / engine.vel_e
+	engine.area_t = engine.mass_dot * engine.vol_t / engine.vel_t
+	engine.area_e = engine.mass_dot * engine.vol_e / engine.vel_e
+	engine.te = engine.tt * (engine.pe/engine.pt)**((engine.k-1)/engine.k)
+
+	print("mass dot = %f" %engine.mass_dot)
+	print("velocity at throat = %f" %engine.vel_t)
+	print("velocity at exit = %f" %engine.vel_e)
+	print("area at exit = %f" %engine.area_t)
+	print("area at exit = %f" %engine.area_e)
+	print("temperature at exit = %f" %engine.te)
+
 def Q_5_1():
 	# ΔV = - c * log(MR)
 	delta_v = 1600
@@ -71,8 +103,9 @@ if __name__ == '__main__':
 	earth = Earth()
 	rocket = Rocket()
 
-	ex_3_1(rocket.engine, earth)
-	ex_3_2(rocket.engine, earth)
-	Q_5_1()
-	Q_5_2()
-	Q_5_3()
+	#ex_3_1(rocket.engine, earth)
+	#ex_3_2(rocket.engine, earth)
+	ex_3_3(rocket.engine, earth)
+	#Q_5_1()
+	#Q_5_2()
+	#Q_5_3()
